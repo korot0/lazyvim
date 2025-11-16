1e62e0f5548a82b7af5ddb2e8ab198ded8651492
korot0 <108902180+korot0@users.noreply.github.com>
2025-11-16 10:46:50 -0600

    Enable line wrapping

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+-- Enable line wrapping
+vim.o.wrap = true -- wrap long lines
+vim.o.linebreak = true -- wrap at word boundaries
+vim.o.showbreak = "→ " -- optional: show an arrow for wrapped lines
