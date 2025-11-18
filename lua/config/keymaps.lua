c1bf2937f6f8f5fea8cb14b04a7875262cae121c
korot0 <108902180+korot0@users.noreply.github.com>
2025-11-17 18:39:50 -0600

    Enable noice pop up with <C-k>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Manual signature help
+vim.api.nvim_set_keymap("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
