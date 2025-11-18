637ced3b520a78e560e257313378d7794a5df51e
korot0 <108902180+korot0@users.noreply.github.com>
2025-11-18 10:38:08 -0600

    Set file explorer to float by default

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,5 @@
 
 -- Manual signature help
 vim.api.nvim_set_keymap("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
+-- Set file explorer to float by default
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree float<cr>", { desc = "File Explorer (float)" })
