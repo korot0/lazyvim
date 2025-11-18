71329ee0d36cbcf9935b4b6fd2537ac8cdb2d5ba
korot0 <108902180+korot0@users.noreply.github.com>
2025-11-17 18:37:06 -0600

    Disable noice pop up

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    lsp = {
+      signature = {
+        auto_open = {
+          enabled = false,  -- turn off automatic popup
+        },
+      },
+    },
+  },
+}
+
