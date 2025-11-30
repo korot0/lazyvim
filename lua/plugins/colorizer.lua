111489a5ac7277f8cca161dd44b89718c60faa02
korot0 <108902180+korot0@users.noreply.github.com>
2025-11-30 16:21:14 -0600

    Add colorizer

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,19 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  event = "VeryLazy",
+  config = function()
+    require("colorizer").setup({
+      filetypes = { "*" },
+      user_default_options = {
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        RRGGBBAA = true,
+        rgb_fn = true,
+        hsl_fn = true,
+        css = true,
+        css_fn = true,
+      },
+    })
+  end,
+}
