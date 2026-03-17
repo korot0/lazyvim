d7cd7e794261cebdf1a5f9c3a6311daad9ff9ed1
Josue Trejo <jt@jt-2.local>
2026-03-17 16:01:42 -0500

    Fix blinking cursor in terminal

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,5 +15,6 @@ vim.opt.spelllang = "en_us"
 
 vim.g.root_spec = { "cwd" }
 
+-- Blinking cursor
 vim.opt.guicursor =
-  "n-v-c:block-blinkwait700-blinkon500-blinkoff500,i-ci-ve:ver25-blinkwait700-blinkon500-blinkoff500,r-cr:hor20,o:hor50"
+  "n-v-c:block-blinkwait700-blinkon500-blinkoff500,i-ci-ve:ver25-blinkwait700-blinkon500-blinkoff500,r-cr:hor20,o:hor50,a:blinkwait700-blinkon500-blinkoff500"
