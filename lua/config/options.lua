26e262d82c2ba5bb886634a8d29116c36a71e68c
Josue Trejo <jt@jt-2.local>
2026-03-17 15:48:02 -0500

    Enable editor blinking cursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,18 +9,10 @@ vim.o.wrap = true -- wrap long lines
 vim.o.linebreak = true -- wrap at word boundaries
 vim.o.showbreak = "→ " -- optional: show an arrow for wrapped lines
 
--- Disable cursor blinking globally
-vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait0-blinkon0-blinkoff0"
-
 -- Spell checker
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
 
 vim.g.root_spec = { "cwd" }
 
-vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait0-blinkon0-blinkoff0"
-  end,
-})
+vim.opt.guicursor = "a:blinkwait700-blinkon500-blinkoff500"
