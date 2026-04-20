f416e79474a9b9d3f8d60cb75617743aadb28503
korot0 <108902180+korot0@users.noreply.github.com>
2026-04-19 21:27:01 -0500

    Add scroll animation

--- lua/plugins/snacks-animated-scrolling-off.lua
+++ lua/plugins/snacks-animated-scrolling-off.lua
@@ -1,8 +1,23 @@
 return {
-	"folke/snacks.nvim",
-	opts = {
-		scroll = {
-			enabled = false, -- Disable scrolling animations
-		},
-	},
+  "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = true,
+      animate = {
+        duration = {
+          step = 10,
+          total = 100,
+        },
+        easing = "linear",
+      },
+      animate_repeat = {
+        delay = 100,
+        duration = {
+          step = 5,
+          total = 50,
+        },
+        easing = "linear",
+      },
+    },
+  },
 }
